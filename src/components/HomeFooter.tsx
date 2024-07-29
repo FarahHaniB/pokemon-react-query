cdcbdfffb938c54d2d166551732c9857179bb5ff
FarahHaniB <109828677+FarahHaniB@users.noreply.github.com>
2024-07-29 14:00:14 +0800

    Edited footer

--- src/components/HomeFooter.tsx
+++ src/components/HomeFooter.tsx
@@ -3,7 +3,7 @@ import { Box, Text } from "@chakra-ui/react";
 
 const HomeFooter = () => {
   return (
-    <Box height='30px' bg='#8594e4' left='0' bottom='0'>
+    <Box h="full" p="15px" bg='#8594e4' left='0' bottom='0'>
       <Text textAlign='center'>
         Pokémon and Pokémon character names are trademarks of Nintendo.
       </Text>
